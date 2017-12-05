a2f38bd157f28453d3b4e9f91047e35b5410e0a4
CY Xu <hello.xu@gmail.com>
2017-12-04 19:05:20 -0800

    stable code, exmaples made

--- img2noise.m
+++ img2noise.m
@@ -12,40 +12,77 @@ clear all;
 close all;
 
 %input image must be color image to get R G B info
-input_img = './output/Picasso_-_Le_pigeon_aux_petits_pois_1911.jpg';
-
-a = imread(input_img);
-[m n r] = size(a);
-
-resizeFactor = 10;
-
-if m >= 1000 || n>= 1000
-    a = imresize(a,1/(5*resizeFactor));
-elseif m >= 300 || n>= 300
-    a = imresize(a,1/(3*resizeFactor));
-elseif m >= 100 || n>= 100
-    a = imresize(a,1/(1*resizeFactor));
-end
-
-[m n r] = size(a);
-
-%a = imgaussfilt(a,2);
-a = im2double(a);
-figure
-imshow(a)
-
+input_img = './output/spiral-rainbow/spiral-rainbow-2.jpg';
 [filepath, name, ext] = fileparts(input_img);
 
-%% reverse every other row of the image
-for i = 1:m
-    if rem(i,2) ~= 0
-        reverse(i,:,:) = a(i,:,:);
-    else
-        reverse(i,:,:) = a(i,end:-1:1,:);
-    end
+a = imread(input_img);
+origImg = im2double(a);
+[oHeight, oWidth, oDepth] = size(origImg);
+
+outputVideo = VideoWriter(fullfile(filepath,name));
+outputVideo.FrameRate = 24;
+open(outputVideo)
+
+[m n r] = size(a);
+
+%%
+figure
+subplot(1,3,1);
+imshow(a);
+refresh
+title('Input Image');
+
+resizeFactor = 1;
+
+if m >= 1000 || n>= 1000
+    resizeFactor = 50;
+    a = imresize(a,1/resizeFactor);
+elseif m >= 300 || n>= 300
+    resizeFactor = 30;
+    a = imresize(a,1/resizeFactor);
+elseif m >= 100 || n>= 100
+    resizeFactor = 10;
+    a = imresize(a,1/resizeFactor);
 end
 
-a = reverse;
+smallA = a;
+
+[m n r] = size(a);
+
+aGray = rgb2gray(a);
+aGray = im2double(aGray);
+overAllTone = sum(aGray(:))/(m*n);
+
+%% Gaussian blur the image to produce a smoother tone?
+%a = imgaussfilt(a,2);
+
+subplot(1,3,2);
+imshow(a);
+title('Processed Image');
+
+a = im2double(a);
+
+
+%% reverse every other row of the image
+% for i = 1:m
+%     if rem(i,2) ~= 0
+%         reverse(i,:,:) = a(i,:,:);
+%     else
+%         reverse(i,:,:) = a(i,end:-1:1,:);
+%     end
+% end
+% 
+% a = reverse;
+
+%% calculate gradient for duration of each pixel
+
+paddedA = padarray(aGray,[0 1],'symmetric','both');
+hor_gradient = abs(paddedA(:,3:end) - paddedA(:,1:end-2));
+reverse_gradient = abs(1 - hor_gradient);
+
+subplot(1,3,3);
+imshow(hor_gradient);
+title('Energy Map');
 
 %% loop through each pixel in image to get RGB
 % use R,G,B as three inputs to the oscillartor
@@ -55,62 +92,53 @@ a = reverse;
     % 'optional 5th argument ''phase'' should be a real number between -2pi and 2pi.'
     % 'optional 4th argument ''gate'' must be a positive number less than or equal to half the duration.'
 
-duration = 1/10;
 sound = [];
 sampleRate = 44100;
+timbre = 'Sinusoid';
 
 for x = 1:m
     for y = 1:n
-        phase = a(x,y,1) * 2 * pi;
-        frequency = (a(x,y,1) + a(x,y,2) + a(x,y,3))/3 * sampleRate / 20;
-        gate = a(x,y,3) * duration / 2;
-        s = oscillator('Sinusoid',duration,frequency,gate,phase,sampleRate);
+        disp(x);
+        
+        phase = a(x,y,1) * 2 * pi; %useless
+%         frequency = (a(x,y,1) + a(x,y,2) + a(x,y,3))/3 * sampleRate / 20;
+        frequency = sum(a(x,y,:));
+%         frequency = frequency * overAllTone * sampleRate / 30;
+        frequency = abs(log10(frequency)) * overAllTone * sampleRate / 10;
+        duration = hor_gradient(x,y) * 2;
+        gate = duration / 2;
+        s = oscillator(timbre,duration,frequency,gate,phase,sampleRate);
         sound = vertcat(sound,s);
+        numFrame = int32(duration*24.0);
+        xstart = (x - 1)*resizeFactor;
+        ystart = (y - 1)*resizeFactor;
+        if xstart == 0
+            xstart = 1;
+        end
+        if ystart == 0
+            ystart = 1;
+        end
+        xend = x*resizeFactor;
+        yend = y*resizeFactor;
+        if xend >  oHeight
+            xend = oHeight;
+        end
+        if yend > oWidth
+            yend = oWidth;
+        end
+        outputImg = origImg;
+        outputImg(xstart:xend, ystart:yend, :) = 1.0 - outputImg(xstart:xend, ystart:yend, :);
+        for ii = 1:numFrame
+            writeVideo(outputVideo,outputImg)
+        end
     end
 end
 
+close(outputVideo)
+
 player = audioplayer(sound,sampleRate);
-play(player);
+%play(player);
 
 %% output
-output_name = strcat(filepath,'/',name,'_Sinusoid','.wav');
+output_name = strcat(filepath,'/',name,'_',timbre,'.wav');
 audiowrite(output_name, sound, 44100);
-
-%% read a sound
-% [y,Fs] = audioread('./output/jingle.wav');
-% y = y(:,1);
-% % dt = 1/Fs;
-% % t = 0 : dt : (length(y) * dt) - dt;
-% % figure
-% % plot(psd(spectrum.periodogram,y,'Fs',Fs,'NFFT',length(y)));
-% % 
-% %sound_fft = fft2(y);
-% player = audioplayer(y,Fs);
-% %play(player);
-
-% %% hmmm...
-% sizedata = [1 numsep(m)/10 1 numsep(n)/10 1 numsep(r)/10 1];
-% 
-% a = double(a);
-% a = (a-127.5)./127.5;
-
-% %% reshape data into one row, reshape(A,sz), prod(sz) must be the same as numel(A).
-% imgdata = reshape(a,1,m*n*r);
-
-% %% what's comdata for?
-% comdata = [sizedata imgdata];
-% 
-% %% one row data
-% [m n] = size(comdata);
-% 
-% %% make sure numel(comdata) is even number
-% if(rem(n,2)~=0)
-%     comdata = [comdata 1];
-% end
-% 
-% %% reshape data to two rows and audiowrite with same input name
-% [m n] = size(comdata);
-% %arrdata = reshape(comdata,n/2,2);
-
-%% padarray comdata to same length as sin sound
-
